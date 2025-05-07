996b823656a4d9d6711d2b9a1905fd3965d0732e
Rad-Vega <198068530+Rad-Vega@users.noreply.github.com>
2025-05-07 15:09:35 +0200

    Revert "Change in User Schema"
    This reverts commit d7af3a44102c1ab7b3333a9e54e9827cc2ecc7ba.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPosts, getPostsByZip, createPost } = require('../controllers/postController');
+const { getAllPosts } = require('../controllers/postController');
 const { authenticate } = require('../middleware/authenticate');
 
 // Get all posts route, authenticates user first
-router.get('/all', authenticate, getAllPosts);
+router.get('/', authenticate, getAllPosts);
 
-router.get('/zip', authenticate, getPostsByZip);
-
-router.post ('/post', authenticate, createPost )
+router.get('/', authenticate, getPostsByZip);
 
 module.exports = router;
