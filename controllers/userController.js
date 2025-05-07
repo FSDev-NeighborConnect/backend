20f9729dfd08c80959b3f3029b56b767624cc5cd
M <findmeenu@gmail.com>
2025-05-07 20:18:05 +0200

    API to get all users by MG

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,17 @@ exports.getUserById = async (req, res, next) =>{
         next(err);
     }
 }
+
+
+exports.getAllUsers = async (req, res, next) => {
+    try {
+        const allUsers = await User.find().select('-password');
+        if(allUsers){
+            return res.status(200).json(allUsers)
+        } else {
+            return res.status(404).json({message: 'No Users found'});
+        } 
+    }catch (err){
+            next(err);
+        }
+    }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require ('express');
 const router = express.Router();
-const {getUserById} = require ('../controllers/userController');
+const {getUserById, getAllUsers} = require ('../controllers/userController');
 const {authenticate} = require ('../middleware/authenticate')
+const {validateUserId} = require('../middleware/validator')
 
 // Route to get all users
-router.get ('/all/users', authenticate, getUSers);
+router.get ('/all/users', authenticate, getAllUsers);
 
+// Route to get user by id
 router.get('/user/:id', authenticate, validateUserId, getUserById);
 
 module.exports = router ;
