2b3ce4077d4936094e8acb791c232f3fe6bbcf8c
Rad-Vega <198068530+Rad-Vega@users.noreply.github.com>
2025-05-07 21:16:10 +0200

    Implement admin-only user DELETE route

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -17,6 +17,22 @@ const adminUpdateUser = async (req, res) => {
   }
 };
 
+const adminDeleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: `User ${user.name} successfully deleted!` });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete user!', error: err.message });
+  }
+};
+
 module.exports = {
+  adminDeleteUser,
   adminUpdateUser
 };
