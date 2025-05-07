394546e719249f36fba6669956bc30b774596b07
M <findmeenu@gmail.com>
2025-05-07 14:48:40 +0200

    Updated Code

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPosts, getPostsByZip, createPost } = require('../controllers/postController');
+const { getAllPosts, getPostsByZip, createPost, getPostById } = require('../controllers/postController');
 const { authenticate } = require('../middleware/authenticate');
 
 // Get all posts route, authenticates user first
-router.get('/all', authenticate, getAllPosts);
+router.get('/all/posts', authenticate, getAllPosts);
 
+//Get posts by zip
 router.get('/zip', authenticate, getPostsByZip);
 
-router.post ('/post', authenticate, createPost )
+//Create a new post
+router.post ('/post', authenticate, createPost)
+
+// Visit a post 
+router.get ('/:id', authenticate, getPostById)
 
 module.exports = router;
