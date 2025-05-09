74c0f967203d4bab63b2a5fe67fc555d849a0e86
Rad-Vega <198068530+Rad-Vega@users.noreply.github.com>
2025-05-09 18:24:00 +0200

    Implement admin-only login route & logic

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { adminUpdateUser, adminDeleteUser, getAllUsers } = require('../controllers/adminController');
 const authenticate = require('../middleware/authenticate');
 const requireAdmin = require('../middleware/adminMiddleware');
+const adminLogin = require('../controllers/adminAuthController');
 
-// Route to get all users
+router.put('/login', adminLogin);
 router.get('/all/users', authenticate, requireAdmin, getAllUsers);
 router.put('/users/:id', authenticate, requireAdmin, adminUpdateUser);
 router.delete('/users/:id', authenticate, requireAdmin, adminDeleteUser);
